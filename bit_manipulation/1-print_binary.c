76d8950a1fa70e99e29ca3051244d6e0e4ed3b41
ReyesxRoyal <rasheed.r.white@gmail.com>
2023-11-13 20:38:52 -0800

    bit_manipulation

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * _pow - func calculates (base ^ power)
+ * @base: base of the exponent
+ * @power: power of the exponent
+ *
+ * Return: value of (base ^ power)
+ */
+unsigned long int _pow(unsigned int base, unsigned int power)
+{
+	unsigned long int num;
+	unsigned int a;
+
+	num = 1;
+	for (a = 1; a <= power; a++)
+		num *= base;
+	return (num);
+}
+
+/**
  * print_binary - prints a number in binary notation
  * @n: number to print
  *

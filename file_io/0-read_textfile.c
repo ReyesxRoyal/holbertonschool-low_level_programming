4098291796955204122e827ef5dc2d392a357134
ReyesxRoyal <rasheed.r.white@gmail.com>
2023-11-13 21:15:48 -0800

    0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
-  * read_textfile - ...
-  * @filename: The source file
-  * @letters: Number of letters to reads and prints
-  *
-  * Return: ...
-  */
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, readed;
